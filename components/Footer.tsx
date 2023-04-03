1270af1093410373e46d6e722fca3bbedd1057e5
Victor Landim <victorlandim5@gmail.com>
2023-04-03 12:15:13 -0500

    update header & footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
 const Footer = () => (
-  <footer className="p-4 text-center text-lg font-bold text-white/80 sm:p-12">
-    victor.photos
+  <footer className="p-6 pt-0 text-center text-xs leading-4 text-white/80">
+    {`${new Date().getFullYear()} `}💻/📷 with ❤ by
+    <br />
+    ∇ictor Landim
   </footer>
 );
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -11,7 +11,9 @@ const listItems = [
 
 const Header = () => (
   <header className="flex items-center justify-between border-b-2 border-white/10 px-6 py-4 text-white/80">
-    <div className="text-left font-bold">victor.photos</div>
+    <div className="text-left font-bold">
+      <Link href="/">∇ictor.photos</Link>
+    </div>
     <ul className="flex">
       {listItems.map((item) => (
         <li className="flex cursor-pointer flex-col items-center hover:underline">
