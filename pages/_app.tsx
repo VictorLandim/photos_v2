d00a756b8152bd8fb304dc5f7064045640a2213f
Victor Landim <victorlandim5@gmail.com>
2023-04-03 12:07:31 -0500

    add new font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from "next/app";
+import { Fragment_Mono } from "next/font/google";
 import "photoswipe/dist/photoswipe.css";
 import "../styles/index.css";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const fragmentMono = Fragment_Mono({ subsets: ["latin"], weight: ["400"] });
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <style jsx global>{`
+      html {
+        font-family: ${fragmentMono.style.fontFamily};
+      }
+    `}</style>
+    <Component {...pageProps} />
+  </>
+);
+
+export default App;
