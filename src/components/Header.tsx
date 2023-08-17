b1f46256ee09e79469548cc142439f3ccf195e92
Victor Landim <victorlandim5@gmail.com>
2023-08-17 17:24:27 +0300

    feat: update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,20 +10,20 @@ const listItems = [
 ];
 
 const Header = () => (
-  <header className="sticky top-0 z-10 flex w-full items-center justify-center border-b-2 border-white/10 bg-black pb-2 pt-3 text-white/80">
+  <header className="sticky top-0 z-10 flex h-[58px] w-full items-center justify-center border-b-2 border-white/10 bg-black text-white/80">
     <div className="flex max-w-[1400px] flex-1 items-center justify-between px-4 sm:px-6">
-      <div className="text-left font-bold">
+      <div className="pt-1 text-left font-bold">
         <Link href="/">∇ictor.photos</Link>
       </div>
       <ul className="flex">
         {listItems.map((item) => (
           <li key={item.href}>
             <Link
-              className="flex cursor-pointer flex-col items-center hover:underline"
+              className="flex cursor-pointer items-center gap-1.5 hover:underline"
               href={item.href}
             >
               {item.icon}
-              <p className="mt-1 text-[0.6rem]">{item.text}</p>
+              <p className="text-xs">{item.text}</p>
             </Link>
           </li>
         ))}
