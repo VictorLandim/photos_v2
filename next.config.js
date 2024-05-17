e5af3b786ce52b022b36253ceeefcf11039096b7
Victor Landim <victorlandim5@gmail.com>
2024-05-17 11:15:29 -0300

    feat: cache images forever

--- next.config.js
+++ next.config.js
@@ -5,8 +5,23 @@ module.exports = {
     domains: ["res.cloudinary.com"],
     loader: "custom",
     loaderFile: "./src/utils/cloudinaryLoader.js",
+    minimumCacheTTL: 1000,
   },
   experimental: {
     appDir: true,
+  },
+  async headers() {
+    return [
+      {
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+        source:
+          "/:path(.+\\.(?:ico|png|svg|jpg|jpeg|avif|gif|webp|json|js|css|mp3|mp4|ttf|ttc|otf|woff|woff2)$)",
+      },
+    ];
   },
 };
