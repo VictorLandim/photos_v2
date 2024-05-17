344d17e2dcd7c5a55fb885ab86105b8737e8ac18
Victor Landim <victorlandim5@gmail.com>
2024-05-17 10:53:07 -0300

    feat: sort z-index map tile on hover, make tiles links

--- src/components/AlbumsMap.tsx
+++ src/components/AlbumsMap.tsx
@@ -14,22 +14,25 @@ export const AlbumsMap = ({ albumList }: { albumList: Item[] }) => {
       return {
         icon: new DivIcon({
           iconSize: [80, 80],
-          className: "opacity-80 hover:opacity-100 transition-all",
+          className:
+            "opacity-80 hover:!z-[99999] hover:opacity-100 transition-all",
           html: `
-        <div class="size-[80px] shadow-lg hover:scale-125 z-0 hover:z-100 transition-all relative flex items-center justify-center h-full w-full rounded-lg border-4 border-white text-black bg-cover bg-center" style="background-image: linear-gradient(to top, rgba(0,0,0,0.99), transparent 60%), url(${cloudinaryLoader(
-          {
-            src: "victorphotos/" + album.name + "/" + album.featuredImagePath,
-            width: 300,
-            quality: 70,
-          }
-        )}) ">
+        <a href="/${
+          album.name
+        }" class="size-[80px] shadow-lg hover:scale-125 transition-all relative flex items-center justify-center h-full w-full rounded-lg border-4 border-white text-black bg-cover bg-center" style="background-image: linear-gradient(to top, rgba(0,0,0,0.99), transparent 60%), url(${cloudinaryLoader(
+            {
+              src: "victorphotos/" + album.name + "/" + album.featuredImagePath,
+              width: 300,
+              quality: 70,
+            }
+          )}) ">
         <div class="absolute -top-3 -right-3 size-6 rounded-full bg-pink-600 text-white font-semibold flex items-center justify-center">${
           album.count
         }</div>
         <div class="absolute bottom-[2px] left-[4px] text-[9px] text-white font-semibold">${
           album.altName
         }</div>
-        </div>
+        </a>
        `,
         }),
         pos: album.pos,
