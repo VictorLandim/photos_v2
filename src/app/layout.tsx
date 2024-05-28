a203a7af1d72d7b6f7d2300e70c2b6f9ca00f528
Victor Landim <victorlandim5@gmail.com>
2024-05-28 14:28:42 -0300

    feat: add ms clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { SunnyShotBanner } from "@/components/SunnyShotBanner";
 import { GridLayoutProvider } from "@/contexts/GridLayoutContext";
 import { Poppins } from "next/font/google";
+import Script from "next/script";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -9,11 +10,23 @@ const font = Poppins({
 
 import "photoswipe/dist/photoswipe.css";
 import "../styles/globals.css";
-// include styles from the ui package
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en" className={`dark bg-black antialiased ${font.className}`}>
+      <Script
+        id="msc"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "mj6pv9arkx");
+                `,
+        }}
+      />
       <body>
         <GridLayoutProvider>
           <div className="flex min-h-screen flex-col bg-gradient-to-b from-gray-100 via-gray-50 to-gray-100 dark:from-black dark:via-neutral-900 dark:to-black">
